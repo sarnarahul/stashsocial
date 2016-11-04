8eacaebba0ec02237ff86831bcb0e0cb1da7ba02
Rahul Sarna <sarnarahul@gmail.com>
2016-11-03 21:06:05 -0400

    Initial Commit

---
+++ StashSocial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StashSocial
+//
+//  Created by Rahul Sarna on 11/3/16.
+//  Copyright © 2016 AutoKrat Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
